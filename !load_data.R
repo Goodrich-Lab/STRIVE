54686fada4a9f132514724c36b2b9bb6dd1dc829
boseo <bjseo2@gmail.com>
2024-06-30 19:02:16 -0400

    amended

--- !load_data.R
+++ !load_data.R
@@ -5,12 +5,30 @@
 
 data <- readxl::read_xlsx(fs::path(
   dir_data,
-  "STRIVE_PFAS_demo_May24.xlsx")) %>% 
+  "STRIVE_PFAS_demo_28jun24.xlsx")) %>% 
   janitor::clean_names()%>%
   mutate(sex = ifelse(sex == 1, "Male", "Female"),
          rural = ifelse(rural == 1, "Living in rural area", "Living in Metro area"),
          smoking = ifelse(smoking == 1, "Smoke or use vape", "Don't smoke or use vape"),
-         case_control = ifelse(case_control == 1, "With cirrhosis", "Healthy"))%>%
+         case_control = ifelse(case_control == 1, "With cirrhosis", "Healthy"),
+         
+         ## added by BS
+         ethnicity = ifelse(ethnicity == 1, "Hispanic", "Not Hispanic"),
+         sq_drink_alcohol = case_when(
+           sq_drink_alcohol == 1 ~ "Yes, current drinker",
+           sq_drink_alcohol == 2 ~ "No, former drinker (stopped)",
+           sq_drink_alcohol == 3 ~ "No, never drinker",
+           TRUE ~ "Unknown/Not Reported"), 
+         sq_average_drink_per_day = case_when(
+             sq_average_drink_per_day == 1 ~ "Less than 1 alcoholic drink per day",
+             sq_average_drink_per_day == 2 ~ "1-2 alcoholic drinks per day",
+             sq_average_drink_per_day == 3 ~ "3-4 alcoholic drinks per day",
+             sq_average_drink_per_day == 4 ~ "More than 4 alcoholic drinks per day",
+             TRUE ~ "Unknown/Not Reported"
+           )
+         ## to here 
+         
+         )%>%
   mutate(rural = ifelse(is.na(rural), "Unknown/Not Reported", rural),
          smoking = ifelse(is.na(smoking), "Unknown/Not Reported", smoking))%>%
   drop_na(case_control)
@@ -36,12 +54,12 @@ emerging_cat <- c("pfbs_median", "pf_pe_a_median","pf_pe_s_median", "pfba_detect
 
 # emerging <- c("pfbs", "pf_pe_a","pf_pe_s", "x9cl_pf3ons")
 
+## added or edited by BS
 covars <- c("source", "age_at_enrollment","sex", 
-            "race_eth_label", "rural", "smoking")
+            "rural", "smoking","race_final_label", "sq_average_drink_per_day")
 
-
-
-covars_analysis <- covars[1:4]
+covars_analysis <- covars[1:7]
+## to here
 
 # Imputation of PFAS: min(pfas concentration)/sqrt(2)
 data_imputed <- data %>%
@@ -59,10 +77,13 @@ data_imputed <- data %>%
 data_scaled <- data_imputed %>% 
   mutate_at(.vars = pfas_name_analysis,
             .funs = list(scld = ~scale(.)%>% as.vector(.)))%>%
+  ## added or edited by BS
   mutate(smoking = ifelse(is.na(smoking), "Unknown", smoking),
          rural = ifelse(is.na(rural), "Unknown", rural),
-         race_eth_label = ifelse(is.na(race_eth_label), "Unknown", race_eth_label))
-
+         race_eth_label = ifelse(is.na(race_eth_label), "Unknown", race_eth_label),
+         race_final_label = ifelse(is.na(race_final_label), "Unknown", race_final_label),
+         sq_average_drink_per_day = ifelse(is.na(sq_average_drink_per_day), "Unknown", sq_average_drink_per_day))
+  ## to here
 
 pfas_name_scld <- paste0(pfas_name_analysis,"_", "scld")
 legacy_scld <- paste0(legacy,"_", "scld")
